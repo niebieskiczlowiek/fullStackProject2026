2ef414b131e677ccb5ca2e258cda9630f2bd740c
niebieskiczlowiek <u19_adaczy_waw@technischools.com>
2026-02-15 16:19:41 +0100

    fixes

--- my-app/services/film.ts
+++ my-app/services/film.ts
@@ -2,7 +2,7 @@ import { Film, FilmSet } from "@/types/film";
 import { apiFetch } from "./client";
 
 export const FilmService = {
-    getNowPlaying: (page: number = 1) => apiFetch<FilmSet>(`movie/now_playing?page=${page}`),
+    getNowPlaying: (page: number = 1) => apiFetch<FilmSet>(`/movie/now_playing?page=${page}`),
     getUpcoming: (page: number = 1) => apiFetch<FilmSet>(`/movie/upcoming?page=${page}`),
     getPopular: (page: number = 1) => apiFetch<Omit<FilmSet, "dates">>(`/movie/popular?page=${page}`),
     getTopRated: (page: number = 1) => apiFetch<Omit<FilmSet, "dates">>(`/movie/top_rated?page=${page}`),
